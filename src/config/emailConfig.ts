cd658cc3d9e0fa969cce756c54d56ba62fdfe424
Cristian-MG00 <cristian_carp2000@hotmail.com>
2026-08-15 01:07:08 -0300

    disable tls

--- src/config/emailConfig.ts
+++ src/config/emailConfig.ts
@@ -4,34 +4,14 @@ const USER = process.env.EMAIL_USER;
 const PASS = process.env.EMAIL_PASS;
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: USER,
     pass: PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
 export { transporter };
 
-// import nodemailer from "nodemailer";
-
-// const USER = process.env.EMAIL_USER;
-// const PASS = process.env.EMAIL_PASS;
-
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: USER,
-//     pass: PASS,
-//   },
-//   tls: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// export { transporter };
